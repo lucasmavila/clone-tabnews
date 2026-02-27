1e6f724b561521dfc36e72286206a52fa06a8e8c
lucasmavila <lucas.mavila94@gmail.com>
2026-02-27 11:45:48 -0300

    add database.js and .env files

---
+++ infra/database.js
@@ -0,0 +1,19 @@
+import { Client } from "pg";
+
+async function query(queryParams) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryParams);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
