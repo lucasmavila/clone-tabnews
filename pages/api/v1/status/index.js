b2e3deb26a3c7ed02e1e8b736413701fede2e0d9
lucasmavila <lucas.mavila94@gmail.com>
2026-03-03 20:02:26 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,31 @@
 import database from "infra/database.js";
-
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1;");
-  console.log(result);
-  response.status(200).json({ message: "Boa deu bom" });
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersion = dbVersionResult.rows[0].server_version;
+
+  const dBMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnections = parseInt(
+    dBMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseActiveConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const dbActiveConnections = databaseActiveConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: dbMaxConnections,
+        opened_connections: dbActiveConnections,
+      },
+    },
+  });
 }
 export default status;
