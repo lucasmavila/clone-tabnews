363b1210cb9ba8d702363367adf1bcb692631f6c
lucasmavila <lucas.mavila94@gmail.com>
2026-03-07 10:31:54 -0300

    handle ssl certificate for supabase

--- infra/database.js
+++ infra/database.js
@@ -1,21 +1,7 @@
 import { Client } from "pg";
 
 async function query(queryParams) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV !== "development" ? true : false,
-  });
-  console.log("CREDENTIALS", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-  });
+  const client = new Client(buildClientConfig());
 
   try {
     await client.connect();
@@ -27,6 +13,23 @@ async function query(queryParams) {
   } finally {
     await client.end();
   }
+}
+
+function buildClientConfig() {
+  const result = {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: false,
+  };
+  if (process.env.NODE_ENV === "development") return result;
+
+  const ca = Buffer.from(process.env.POSTGRES_CA, "base64").toString("utf8");
+  result.ssl = { ca, rejectUnauthorized: true };
+
+  return result;
 }
 
 export default {
